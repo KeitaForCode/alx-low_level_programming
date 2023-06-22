13d0045a10f5f56a3edfb6fb2251b550e8284ea5
KeitaForCode <keita.losene@yahoo.com>
2023-06-22 05:58:25 -0700

    0x03-debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Causes infinite loop.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("incoming infinite loop: (\n");
+	
+	i = 0;
+
+	/* while(i < 10) */
+	/*{*/
+	 /*	putchar(i);*/		
+	 /* }*/
+
+	printf("Infinite loop avoided ! \\o/\n");
+	return (0);
+}
